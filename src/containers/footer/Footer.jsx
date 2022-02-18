b26e17bdffd15033eb3ce70e7d1351485bc730da
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-02-18 22:06:37 +0100

    new update

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>Plot 49, New Avenue rd.</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms &#38; Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Plot 49, New Avenue rd.</p>
+        <p>111-123-456-89</p>
+        <p>info@gpt3.net</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>&copy;{new Date().getFullYear()} GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
