785d51dbc97baee69043cfeb9eb1173297abcf79
Isaac Oyedele <isaacoyedele@hotmail.com>
2022-03-03 12:35:11 +0100

    update to querySelector

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.querrySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
